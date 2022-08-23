c908ae2bc383ba85c9ad824bde719a03c5c7acca
Frizky Afif <frizky.afif@mncbank.co.id>
2022-08-23 19:35:08 +0700

    Tugas 1

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "Genap")
+		} else {
+			fmt.Println(i, "Ganjil")
+		}
+	}
 }
